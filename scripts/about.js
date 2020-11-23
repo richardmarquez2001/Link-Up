59211600892d63733557e0c361d3d4b595b4f81c
xvxvdee <deandra.spikemadden@ryerson.ca>
2020-11-22 20:50:20 -0500

    Added text to about page, images for the about page to the page and images folder, finished slideshow using js. About page 90% complete.

--- scripts/about.js
+++ scripts/about.js
@@ -3,103 +3,83 @@ let left = document.getElementById("lClick");
 let right = document.getElementById("rClick");
 
 //Bubbles
-let b1 = document.querySelector(".p1");
+let b1 = document.getElementById("p1");
 let b2 = document.getElementById("p2");
-let b3 = document.getElementById("p3");
 let b4 = document.getElementById("p4");
 let b5 = document.getElementById("p5");
 let b6 = document.getElementById("p6");
-let b7 = document.getElementById("p7");
 
 //Information to change
 let text = document.getElementById("infoText");
 let title = document.getElementById("infoTitle");
 let image = document.getElementById("infoImage");
 
-console.log(text.innerHTML);
-console.log(title.innerHTML);
-console.log(image);
 
 let position = 0;
 let info_title = [
   "It's Free!",
   "It's Fast!",
-  "It's Modern!",
-  "There are no Strings-Attached!",
   "It's Easy!",
   "It's Simple!",
   "It's Safe!",
 ];
 let info_text = [
-  "It's Free!: RESPONSE",
-  "It's Fast!: RESPONSE",
-  "It's Modern!: RESPONSE",
-  "There are no Strings-Attached!: RESPONSE",
-  "It's Easy!: RESPONSE",
-  "It's Simple!: RESPONSE",
+  "You don't have to worry about paying any membership fee when using Link Up. Sign up for free and start making friends easily!",
+  "Don't worry about long sign up processes or a limited about of searches per day. Get access with in minutes and match with people who have the same interest as you.",
+  "Make friends at the same rate you could offline. Answer a few carefully selected questions and be matched with people instantly.",
+  "Get comfortable learning how to use Link Up within a matter of seconds. Our interface is straightforward and leaves no room for you to get confused!",
   "It's Safe!: RESPONSE",
 ];
-// let src_images = []
+let src_images = [
+  "/Link-Up/images/aboutp1.jpeg",
+  "/Link-Up/images/aboutp4.jpg",
+  "/Link-Up/images/aboutp2.jpg",
+  "/Link-Up/images/aboutp3.jpeg",
+  "/Link-Up/images/aboutp5.jpg"
+];
+
 function slideLeft() {
-    test
   console.log("left");
   position -= 1;
   if (position == -1) {
-    position = 6;
+    position = 4;
   }
-//   bounce(position);
+  changeColor(position);
   text.innerHTML = info_text[position];
   title.innerHTML = info_title[position];
+  image.setAttribute("src",src_images[position])
 }
 
 function slideRight() {
   console.log("right");
 
   position += 1;
-  if (position == 7) {
+  if (position == 5) {
     position = 0;
   }
-//   bounce(position);
+  changeColor(position);
   text.innerHTML = info_text[position];
   title.innerHTML = info_title[position];
+  image.setAttribute("src",src_images[position])
+
 }
 
-function test(){
-    document.getElementById("p6").style="animation:bounce_1; animation duration: 5s;"
-}
-
-function clearAnimation(){
-    b1.classList.remove("animate");
-    b2.classList.remove("animate");
-    b3.classList.remove("animate");
-    b4.classList.remove("animate");
-    b5.classList.remove("animate");
-    b6.classList.remove("animate");
-    b7.classList.remove("animate");
-}
-
-function bounce(position) {
+function changeColor(position) {
   switch (position) {
     case 0:
-      b1.classList.add("animate");
+      title.setAttribute("style","background-color:#B4A0E5;");
       break;
     case 1:
-      b2.classList.add("animate");
+      title.setAttribute("style","background-color:#8AE9C1;");
       break;
     case 2:
-      b3.classList.add("animate");
+      title.setAttribute("style","background-color:#FC6471;");
       break;
     case 3:
-      b4.classList.add("animate");
+      title.setAttribute("style","background-color:#8AE9C1;");
       break;
     case 4:
-      b5.classList.add("animate");
-      break;
-    case 5:
-      b6.classList.add("animate");
-      break;
-    case 6:
-      b7.classList.add("animate");
+      title.setAttribute("style","background-color:#B4A0E5;");
       break;
   }
 }
